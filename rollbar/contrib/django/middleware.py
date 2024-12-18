70ad025a908af03419ee71a2d9a78492669b94b2
Mike Manger <mike.manger@lighthouseuk.net>
2024-12-18 15:21:07 +0000

    Remove import check for django resolve() method.
    This was changed in 1.10

--- rollbar/contrib/django/middleware.py
+++ rollbar/contrib/django/middleware.py
@@ -83,14 +83,10 @@ import sys
 
 import rollbar
 
-from django.core.exceptions import MiddlewareNotUsed
 from django.conf import settings
+from django.core.exceptions import MiddlewareNotUsed
 from django.http import Http404
-
-try:
-    from django.urls import resolve
-except ImportError:
-    from django.core.urlresolvers import resolve
+from django.urls import resolve
 
 try:
     from django.utils.deprecation import MiddlewareMixin
